525b050fb905eb57fbbca4a8846831d20fae1576
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-11-01 13:18:20 +0100

    Variables declaration on top of script
    Move all of your variable declarations to the top of the script

--- js/math.js
+++ js/math.js
@@ -1,16 +1,18 @@
 // declare program variables
-
+let num1,
+    num2,
+    message;
 // announce the program
 alert("Let's do some math!");
 
 // collect numeric input
-var num1 = prompt("Please type a number");
+num1 = prompt("Please type a number");
 num1 = parseFloat(num1);
-var num2 = prompt("Please type another number");
+num2 = prompt("Please type another number");
 num2 = parseFloat(num2);
 
 // build an HTML message
-var message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
+message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
 message += num1 + " + " + num2 + " = " + (num1 + num2);
 message += "<br>";
 message += num1 + " * " + num2 + " = " + (num1 * num2);

203167769f67b87ce376338d24fa8cd45bdf1209
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-11-02 09:58:25 +0100

    Update js/math.js
    Don't build the message variable, or write the message to the doc, if
       either case 2 or 3 above fails.

--- js/math.js
+++ js/math.js
@@ -16,23 +16,21 @@ num2 = parseFloat(num2);
 if(num2 == 0) {
     alert("The second number is 0. You can't divide by zero. Reload and try again.");
     document.location.reload(true);
-}
-
-if(isNaN(num1) || isNaN(num2)) {
+} else if(isNaN(num1) || isNaN(num2)) {
     alert("At least one of the values you typed is not a number. Reload and try again.");
     document.location.reload(true);
+} else {
+    // build an HTML message
+    message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
+    message += num1 + " + " + num2 + " = " + (num1 + num2);
+    message += "<br>";
+    message += num1 + " * " + num2 + " = " + (num1 * num2);
+    message += "<br>";
+    message += num1 + " / " + num2 + " = " + (num1 / num2);
+    message += "<br>";
+    message += num1 + " - " + num2 + " = " + (num1 - num2);
+    
+    // write message to web page
+    document.write(message);
 }
-
-// build an HTML message
-message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
-message += num1 + " + " + num2 + " = " + (num1 + num2);
-message += "<br>";
-message += num1 + " * " + num2 + " = " + (num1 * num2);
-message += "<br>";
-message += num1 + " / " + num2 + " = " + (num1 / num2);
-message += "<br>";
-message += num1 + " - " + num2 + " = " + (num1 - num2);
-  
-// write message to web page
-document.write(message);
 
